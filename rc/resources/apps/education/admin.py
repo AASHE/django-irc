5f82a6b7d4f48b44137ad7c5f55326bcb78efdb7
Jesse Legg <jesse.legg@aashe.org>
2012-03-08 11:22:55 -0500

    Removed username from bitbucket URL and added Sphinx to requirements

---
+++ rc/resources/apps/education/admin.py
@@ -0,0 +1,18 @@
+from django.contrib import admin
+from rc.resources.apps.education.models import *
+
+
+admin.site.register(CommunityGarden)
+admin.site.register(SustainabilityNetworks)
+admin.site.register(CampusAgriculture)
+admin.site.register(LivingGuides)
+admin.site.register(SustainabilityMaps)
+admin.site.register(StudentPeerOutreach)
+admin.site.register(CampusSustainabilityCourse)
+admin.site.register(SustainabilityCourseInventory)
+admin.site.register(SustainabilitySyllabus)
+admin.site.register(FacultyWorkshops)
+admin.site.register(SurveyOfAwareness)
+admin.site.register(ResearchInventories)
+admin.site.register(AcademicCenter)
+admin.site.register(AcademicCenterType)
