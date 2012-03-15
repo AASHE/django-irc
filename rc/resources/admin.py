0d4bdaf892bf3c5edbc1bac368c81bbca1b4d6c1
Jesse Legg <jesse.legg@aashe.org>
2012-03-15 11:34:31 -0400

    Removed a few more editable fields from ResourceItemAdmin in prep for staff review

--- rc/resources/admin.py
+++ rc/resources/admin.py
@@ -2,7 +2,7 @@ from django.contrib import admin
 
 
 class ResourceItemAdmin(admin.ModelAdmin):
-    list_display = ('__unicode__', 'title', 'url', 'organization', 'notes', 'published')
+    list_display = ('__unicode__', 'url', 'organization', 'published')
     list_filter = ('published',)
-    list_editable = ['title', 'url', 'organization', 'notes']
+    list_editable = ['url']
     
