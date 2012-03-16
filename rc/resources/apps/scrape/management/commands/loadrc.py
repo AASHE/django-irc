cf1576872141f31825d70f8c19086ff837f10b37
Robert Erb <rerb@users.noreply.github.com>
2012-03-15 20:12:36 -0400

    Added --category and --models options to rcload. Unnamed args are still treated as models to load, as before.

--- rc/resources/apps/scrape/management/commands/loadrc.py
+++ rc/resources/apps/scrape/management/commands/loadrc.py
@@ -1,11 +1,29 @@
+from optparse import make_option
+
 from django.core.management.base import BaseCommand, CommandError
 
-from rc.resources.apps.scrape.process import process_all, process_some
+from rc.resources.apps.scrape.process import process_all, process_some, process_category
+
 
 class Command(BaseCommand):
+        
+    option_list = BaseCommand.option_list + (
+        make_option('--category', action='store', dest='category_name', 
+                    help='Category of models to load'),
+        make_option('--models', action='store', dest='model_names',
+                    help='Models to load') )
+
     def handle(self, *args, **options):
         if args:
-            process_some(args)
+            arg_string = ' '.join(args)
+            options['model_names'] = (' '.join((options['model_names'], arg_string)) 
+                                      if options['model_names'] 
+                                      else arg_string)
+        if options['model_names'] or options['category_name']:
+            if options['model_names']:
+                process_some(options['model_names'].split())
+            if options['category_name']:
+                process_category(options['category_name'])
         else:
             process_all()
         
