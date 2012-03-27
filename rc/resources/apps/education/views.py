c284cc842e647356f30b7d7f681fc72b59b73cad
Robert Erb <rerb@users.noreply.github.com>
2012-03-27 10:10:50 -0400

    replaced views for resources with shared ResourceItemTablesByOrgCountryView

--- rc/resources/apps/education/views.py
+++ rc/resources/apps/education/views.py
@@ -2,40 +2,12 @@ from rc.resources.views import ResourceItemListView
 import rc.resources.apps.education.models as edmodels
 
 
-FILTER_GARDENS_WITH_NO_ORGANIZATION = True
+FILTER_RESOURCES_WITH_NO_ORGANIZATION = True
 
-class CommunityGardenListView(ResourceItemListView):
-
-    context_object_name = 'communitygardens'
-    model = edmodels.CommunityGarden
-
-    def get_context_data(self, **kwargs):
-        context = super(CommunityGardenListView, self).get_context_data(
-            **kwargs)
-
-        context['member_only'] = True
-
-        context['opening_text'] = """
-        This resource lists campus and campus-community gardens that
-        use organic gardening techniques and have a student learning
-        component. Additionally, refer to <a
-        href="http://www.bamco.com/uploads/documents/student_garden_guide_final_-_food_service.pdf">Bon
-        Appetit's Student Gardens Guide</a> and <a
-        href="http://www.youtube.com/watch?v=2eAYgF57Vrw">related
-        video</a> for information and guidance on establishing a
-        garden on your campus."""
-
-        context['guts'] = 'education/communitygarden_list_guts.html'
-    
-        context['closing_text'] = """
-        <em>Please email <a href="mailto:resources@aashe.org">
-        resources@aashe.org</a> with any suggestions, additions or 
-        updates.</em>"""
-
-        return context
+class ResourceItemTablesByOrgCountryView(ResourceItemListView):        
 
     def get_queryset(self, **kwargs):
-        if FILTER_GARDENS_WITH_NO_ORGANIZATION:
+        if FILTER_RESOURCES_WITH_NO_ORGANIZATION:
             return self.model.objects.all().filter(
                 organization__isnull=False).order_by(
                     'organization__country', 'organization__name')
@@ -43,32 +15,3 @@ class CommunityGardenListView(ResourceItemListView):
             return self.model.objects.all().order_by(
                 'organization__country', 'organization__name')
 
-        
-class CampusAgricultureListView(ResourceItemListView):
-
-    model = edmodels.CampusAgriculture
-    
-    def get_context_data(self, **kwargs):
-        context = super(CampusAgricultureListView, self).get_context_data(
-            **kwargs)
-
-        context['member_only'] = True
-
-        context['opening_text'] = """
-            This resource is a collection of campus farms and
-            campus supported agriculture programs that are sources of
-            sustainable food for the campus community. While produce
-            from campus community gardens is usually reserved for use
-            by plot-owners, CSA farms and programs supply locally
-            grown food to campus eateries and members of the campus
-            community."""
-
-        context['guts'] = 'education/campusagriculture_list_guts.html'
-    
-        context['closing_text'] = """
-            <em>Please email <a href="mailto:resources@aashe.org">
-            resources@aashe.org</a> with any suggestions, additions or 
-            updates.</em>"""
-
-        return context
-        
--- rc/resources/urls.py
+++ rc/resources/urls.py
@@ -1,13 +1,17 @@
 from django.conf.urls.defaults import patterns, include, url
 
 import rc.resources.apps.education.views as edviews
+import rc.resources.apps.education.models as edmodels
 
 urlpatterns = patterns(
     'resources',
-    url(r'campus-and-campus-community-gardens', 
-        edviews.CommunityGardenListView.as_view()),
-    url(r'campus-supported-agriculture-and-farms',
-        edviews.CampusAgricultureListView.as_view()),
+    url(r'^campus-and-campus-community-gardens$', 
+        edviews.ResourceItemTablesByOrgCountryView.as_view(
+            model=edmodels.CommunityGarden)),
+    url(r'^campus-supported-agriculture-and-farms$',
+        edviews.ResourceItemTablesByOrgCountryView.as_view(
+            model=edmodels.CampusAgriculture),
+        {'member_only': True}),
     )
 
 """
