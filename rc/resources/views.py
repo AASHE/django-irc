7b767f7b041637a5562fd25586d031f28232ccb5
Robert Erb <rerb@users.noreply.github.com>
2012-03-27 10:09:53 -0400

    hacked member_only flag into context -- what's a better way?

--- rc/resources/views.py
+++ rc/resources/views.py
@@ -1,17 +1,16 @@
 from django.views.generic import ListView
 
+from rc.resources.apps.education.models import CommunityGarden
 
 class ResourceItemListView(ListView):
-
-    # model must be overridden by subclass
-    model = None
-    template_name = 'resourceitem_list.html'
 
     def get_context_data(self, **kwargs):
         context = super(ResourceItemListView, self).get_context_data(
             **kwargs)
 
         context['title'] = self.model._meta.verbose_name_plural.title()
+        if 'member_only' in self.kwargs:
+            context['member_only'] = self.kwargs['member_only']
 
         return context
 
