7632c8b166fad6bc055db86b8aa4cfc505a5edb4
Robert Erb <rerb@users.noreply.github.com>
2012-03-27 14:54:33 -0400

    added simple test for http responses in resources/tests.py.

--- rc/resources/tests.py
+++ rc/resources/tests.py
@@ -5,7 +5,10 @@ when you run "manage.py test".
 Replace this with more appropriate tests for your application.
 """
 
-from django.test import TestCase
+from django.test import TestCase, Client
+
+
+TEST_SERVER = 'localhost:8000'
 
 
 class SimpleTest(TestCase):
@@ -14,3 +17,24 @@ class SimpleTest(TestCase):
         Tests that 1 + 1 always equals 2.
         """
         self.assertEqual(1 + 1, 2)
+
+
+class ResourceUrlsTest(TestCase):
+
+    def get_url_strings(self):
+        import urls
+        url_strings = list()
+        for pattern in urls.urlpatterns:
+            url_strings.append(str(pattern).split()[-1].translate(None, '^$>'))
+        return url_strings
+
+    def test_no_404(self):
+        '''
+        Tests that url is available.
+        '''
+        client = Client()
+        for url in self.get_url_strings():
+            print 'getting', url
+            resp = client.get('/'.join(('http:/', TEST_SERVER, url)))
+            self.assertEqual(resp.status_code, 200)
+
