87a55ee4c8dca5cb5ad98b73b02a3cc717abf150
Robert Erb <rerb@users.noreply.github.com>
2012-03-28 16:23:45 -0400

    Moved query filtering from ResourceItemListView into urls.py.

--- rc/resources/urls.py
+++ rc/resources/urls.py
@@ -3,55 +3,104 @@ from django.conf.urls.defaults import patterns, include, url
 from rc.resources.views import ResourceItemListView
 import rc.resources.apps.education.models as edmodels
 
-urlpatterns = patterns(
-    '',
+
+HIDE_RESOURCES_WITH_NO_ORGANIZATION = False
+
+
+def handle_missing_organizations(qs):
+    if HIDE_RESOURCES_WITH_NO_ORGANIZATION:
+        qs = qs.exclude(organization=None)
+    return qs
+
+urlpatterns = patterns('',
+
     url(r'^resources/alumni-sustainability-networks$',
-        ResourceItemListView.as_view(model=edmodels.SustainabilityNetwork),
+        ResourceItemListView.as_view(
+            model=edmodels.SustainabilityNetwork,
+            queryset=handle_missing_organizations(
+                edmodels.SustainabilityNetwork.objects.all())),
         {'member_only': True}),
+
     url(r'^resources/campus-and-campus-community-gardens$', 
-        ResourceItemListView.as_view(model=edmodels.CommunityGarden),
-        {'member_only': True,
-         'order_by': ('organization__country', 'organization__name')}),
+        ResourceItemListView.as_view(
+            model=edmodels.CommunityGarden,
+            queryset=handle_missing_organizations(
+                edmodels.CommunityGarden.objects.order_by(
+                'organization__country', 'organization__name'))),
+        {'member_only': True}),
+
     url(r'^resources/campus-supported-agriculture-and-farms$',
-        ResourceItemListView.as_view(model=edmodels.CampusAgriculture),
-        {'member_only': True,
-         'order_by': ('organization__country', 'organization__name')}),
+        ResourceItemListView.as_view(
+            model=edmodels.CampusAgriculture,
+            queryset=handle_missing_organizations(
+                edmodels.CampusAgriculture.objects.order_by(
+                 'organization__country', 'organization__name'))),
+        {'member_only': True}),
+
     url(r'^resources/campus-sustainable-living-guides$',
-        ResourceItemListView.as_view(model=edmodels.LivingGuide), 
-        {'member_only': True,
-         'order_by': ('organization__country', 'organization__name'),
-         'exclude_resources_with_no_organization': True}),
+        ResourceItemListView.as_view(
+            model=edmodels.LivingGuide,
+            queryset=handle_missing_organizations(
+                edmodels.LivingGuide.objects.order_by(
+                 'organization__country', 'organization__name'))),
+        {'member_only': True}),
+        
     url(r'^resources/campus-sustainability-mapstours$',
-        ResourceItemListView.as_view(model=edmodels.SustainabilityMap),
-        {'order_by': ('organization__name',)}),
+        ResourceItemListView.as_view(
+            model=edmodels.SustainabilityMap,
+            queryset=handle_missing_organizations(
+                edmodels.SustainabilityMap.objects.order_by(
+                 'organization__name')))),
+
     url(r'^resources/surveys-sustainability-awareness-attitudes-and-values$',
-        ResourceItemListView.as_view(model=edmodels.SurveyOfAwareness),
-        {'member_only': True,
-         'order_by': ('organization__name',)}),
+        ResourceItemListView.as_view(
+            model=edmodels.SurveyOfAwareness,
+            queryset=handle_missing_organizations(
+                edmodels.SurveyOfAwareness.objects.order_by(
+                 'organization__name'))),
+        {'member_only': True}),
+
     url(r'^resources/sustainability-course-inventories$',
         ResourceItemListView.as_view(
-            model=edmodels.SustainabilityCourseInventory),
-        {'member_only': True,
-         'order_by': ('organization__name',)}),
-    url(r'^resources/sustainability-faculty-development-workshops$',
-        ResourceItemListView.as_view(model=edmodels.FacultyWorkshop),
-        {'member_only': True,
-         'order_by': ('organization__name',)}),
-    url(r'^resources/sustainability-research-inventories$',
-        ResourceItemListView.as_view(model=edmodels.ResearchInventory),
-        {'member_only': True,
-         'order_by': ('organization__name',)}),            
-    url(r'^resources/sustainability-related-syllabi-databases$',
-        ResourceItemListView.as_view(model=edmodels.SustainabilitySyllabus),
-        {'member_only': True,
-         'order_by': ('organization__name',),
-         'exclude_resources_with_no_organization': True}),
+            model=edmodels.SustainabilityCourseInventory,
+            queryset=handle_missing_organizations(
+                edmodels.SustainabilityCourseInventory.objects.order_by(
+                'organization__name'))),
+        {'member_only': True}),
 
-    url('^resources/academic-centers-and-research-initiatives-sustainable-agriculture$',
-        ResourceItemListView.as_view(model=edmodels.AcademicCenter),
-        {'order_by': ('organization__name',),
-         'filter': ('academiccentertype__type="AG"',),
-         'template_name': 'academiccenteronsustainableagriculture_list.html'}),
-         
+    url(r'^resources/sustainability-faculty-development-workshops$',
+        ResourceItemListView.as_view(
+            model=edmodels.FacultyWorkshop,
+            queryset=handle_missing_organizations(
+                edmodels.FacultyWorkshop.objects.order_by(
+                 'organization__name'))),
+        {'member_only': True}),
+
+    url(r'^resources/sustainability-research-inventories$',
+        ResourceItemListView.as_view(
+            model=edmodels.ResearchInventory,
+            queryset=handle_missing_organizations(
+                edmodels.ResearchInventory.objects.order_by(
+                 'organization__name'))),
+        {'member_only': True}),
+
+    url(r'^resources/sustainability-related-syllabi-databases$',
+        ResourceItemListView.as_view(
+            model=edmodels.SustainabilitySyllabus,
+            queryset=handle_missing_organizations(
+                edmodels.SustainabilitySyllabus.objects.order_by(
+                    'organization__name'))),
+        {'member_only': True}),
+        
+    url('^resources/academic-centers-and-research-initiatives-sustainable-'
+        'agriculture$',
+        ResourceItemListView.as_view(
+            model=edmodels.AcademicCenter,
+            queryset=handle_missing_organizations(
+                edmodels.AcademicCenter.objects.filter(
+                    type__type='AG').order_by('organization__name')),
+            template_name='education/'
+                          'academiccenteronsustainableagriculture_list.html')),
+                          
     )
 
--- rc/resources/views.py
+++ rc/resources/views.py
@@ -10,14 +10,7 @@ class ResourceItemListView(ListView):
     effective:
 
       - member_only: a boolean, specifies if the view is restricted
-
-      - order_by: a tuple, specifies query set sorting
-
-      - exclude_resources_with_no_organization: a boolean, when True
-        resources with no related organization are excluded from the
-        query set
     '''
-
     def get_context_data(self, **kwargs):
         context = super(ResourceItemListView, self).get_context_data(
             **kwargs)
@@ -27,14 +20,3 @@ class ResourceItemListView(ListView):
             context['member_only'] = self.kwargs['member_only']
 
         return context
-
-    def get_queryset(self, **kwargs):
-        qs = self.model.objects.all()
-        if 'order_by' in self.kwargs:
-            for order in self.kwargs['order_by']:
-                qs = qs.order_by(order)
-        if 'exclude_resources_with_no_organization' in self.kwargs:
-            qs = qs.exclude(organization__isnull=self.kwargs[
-                'exclude_resources_with_no_organization'])
-        return qs
-            
