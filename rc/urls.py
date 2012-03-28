7cb140cb7b097f2db343aaf6e46ffb3caa723ece
Robert Erb <rerb@users.noreply.github.com>
2012-03-28 13:47:26 -0400

    fixed url botch of previous commit.

--- rc/urls.py
+++ rc/urls.py
@@ -10,5 +10,5 @@ urlpatterns = patterns(
     url(r'^admin/doc/', include('django.contrib.admindocs.urls')),
     url(r'^admin/', include(admin.site.urls)),
     (r'^', include('rc.cms.urls')),
-    (r'^resources', include('rc.resources.urls'))    
+    (r'^', include('rc.resources.urls'))    
 )
