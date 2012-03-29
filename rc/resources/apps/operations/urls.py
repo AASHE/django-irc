6c78bdfa04a3ae2a672c2bed2768389ddbccf2a5
Robert Erb <rerb@users.noreply.github.com>
2012-03-29 16:41:44 -0400

    Refactored education resource urls into rc.resources.apps.education.urls.py. Created rc.resources.apps.operations.urls.py to hold operations resource urls.  Tweaked chrometests accordingly.

---
+++ rc/resources/apps/operations/urls.py
@@ -0,0 +1,9 @@
+from django.conf.urls.defaults import patterns, include, url
+
+from rc.resources.views import ResourceItemListView
+from rc.resources.apps.education import models
+
+
+urlpatterns = patterns('',
+    )
+
