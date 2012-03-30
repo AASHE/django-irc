8293516066ec4d34fd58232f98b555a95ab1b35f
Robert Erb <rerb@users.noreply.github.com>
2012-03-29 22:32:40 -0400

    get_context_data now pops all kwargs provided to __init__ up into context. Allows me to pass BottledWaterBan.BAN_TYPES to template.

--- rc/resources/views.py
+++ rc/resources/views.py
@@ -5,18 +5,14 @@ from rc.resources.apps.education.models import CommunityGarden
 
 
 class ResourceItemListView(ListView):
-    '''
-    In the context dict passed to as_view(), the following keys are 
-    effective:
 
-      - member_only: a boolean, specifies if the view is restricted
-    '''
     def get_context_data(self, **kwargs):
         context = super(ResourceItemListView, self).get_context_data(
             **kwargs)
 
         context['title'] = self.model._meta.verbose_name_plural.title()
-        if 'member_only' in self.kwargs:
-            context['member_only'] = self.kwargs['member_only']
+
+        # So we can pass values from urls.py to templates:
+        context.update(dict(self.kwargs))  # Must be a better way?
 
         return context
