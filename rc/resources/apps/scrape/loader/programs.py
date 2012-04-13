b90361390889f2370342d93dde50c8ed0ef5d5f4
Robert Erb <rerb@users.noreply.github.com>
2012-04-13 16:24:22 -0400

    Added comments.

--- rc/resources/apps/scrape/loader/programs.py
+++ rc/resources/apps/scrape/loader/programs.py
@@ -3,11 +3,14 @@ from rc.resources.apps.scrape.loader import GenericLoader
 
 
 class ProgramLoader(GenericLoader):
-    
+
     def create_instance(self, data):
+        '''
+        Links a ProgramType, based on data['program_type'], to the
+        Program that GenericLoader.create_instance() will make.
+        '''
         type_obj, created = models.ProgramType.objects.get_or_create(
             type=data['program_type'])
         data['type'] = type_obj
         del data['program_type']
-        return super(ProgramLoader, self).create_instance(data)        
-
+        return super(ProgramLoader, self).create_instance(data)
