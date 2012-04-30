ddc2c306295cf6b70c59ee63b31b6bec94912928
Matt <mthomas@aashe.org>
2012-04-30 08:45:49 -0400

    fixing urls.py

--- rc/urls.py
+++ rc/urls.py
@@ -1,6 +1,12 @@
-from django.conf.urls.defaults import patterns, include
+from django.conf.urls.defaults import patterns, include, url
+from django.contrib import admin
+
+admin.autodiscover()
 
 urlpatterns = patterns('',
+    url(r'^admin/doc/', include('django.contrib.admindocs.urls')),
+    url(r'^admin/', include(admin.site.urls)),
+    (r'^', include('rc.cms.urls')),
     (r'^', include('rc.resources.apps.education.urls')),
     (r'^', include('rc.resources.apps.operations.urls')),
     (r'^', include('rc.resources.apps.pae.urls')),
