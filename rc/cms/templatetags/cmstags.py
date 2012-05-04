e6eb3dbde3bfcbfa17e01faf2c3c0f16c528ab87
Matt <mthomas@aashe.org>
2012-05-04 15:21:59 -0400

    new fixture data, few minor fixes to menu and cmstags

--- rc/cms/templatetags/cmstags.py
+++ rc/cms/templatetags/cmstags.py
@@ -32,19 +32,22 @@ def generate_breadcrumb(object, request):
     else:
         view = resolve(request.path)
         named_url = view.url_name
-        menuitem = MenuItem.objects.filter(named_url=named_url)[0]
-        menu = menuitem.menu
-        itemslist = []
-        itemslist.append(menuitem)
-        while menuitem != menu.root_item:
-            menuitem = menuitem.parent
-            itemslist.insert(0, menuitem)
-        first, second = itemslist[1], itemslist[-1]
-        if first != second:
-          itemslist = [first, second]
-        else:
-          itemslist = [first]
-        return render_to_string('treemenus/breadcrumb_fragment.html', {'itemslist': itemslist})
+        try:
+            menuitem = MenuItem.objects.filter(named_url=named_url)[0]
+            menu = menuitem.menu
+            itemslist = []
+            itemslist.append(menuitem)
+            while menuitem != menu.root_item:
+                menuitem = menuitem.parent
+                itemslist.insert(0, menuitem)
+            first, second = itemslist[1], itemslist[-1]
+            if first != second:
+              itemslist = [first, second]
+            else:
+              itemslist = [first]
+            return render_to_string('treemenus/breadcrumb_fragment.html', {'itemslist': itemslist})
+        except:
+            pass
 
 register.filter('generate_breadcrumb', generate_breadcrumb)
 
@@ -58,37 +61,40 @@ def generate_sidebar(object, request):
         # get view and named url
         view = resolve(request.path)
         named_url = view.url_name
-        menuitem = MenuItem.objects.filter(named_url=named_url)[0]
-        menu = menuitem.menu
-        # create lists
-        # item list: all menu items
-        # childlist: submenu items
-        itemslist = []
-        childlist = []
-        # populate top-level menu
-        for child in menu.root_item.children():
-            itemslist.append(child)
-        # populate submenu
-        # no need to include stuff that's already in itemlist
-        while menuitem.level > 1:
-            # populate and sort the submenu children
-            if menuitem.level == 2:
-                # activeitem is used to show active item in the sidebar
-                activeitem = menuitem
-                # get siblings of current menu item
-                for sibling in menuitem.siblings():
-                   childlist.append(sibling)
-                childlist.append(activeitem)
-                # sort the list
-                childlist.sort(key=lambda x: x.rank)
-                # get the parent, though it isn't shown it is used for display purposes
-                menuitem = menuitem.parent
-                # don't change this, the parent has to be at the beginning of the list
-                childlist.insert(0, menuitem)
-            # if we're deeper than level 2, go up but don't add the item to the list
-            elif menuitem.level > 2:
-                menuitem = menuitem.parent
-        return render_to_string('treemenus/sidebar_fragment.html', {'itemslist': itemslist, 'childlist': childlist, 'activeitem': activeitem})
+        try:
+            menuitem = MenuItem.objects.filter(named_url=named_url)[0]
+            menu = menuitem.menu
+            # create lists
+            # item list: all menu items
+            # childlist: submenu items
+            itemslist = []
+            childlist = []
+            # populate top-level menu
+            for child in menu.root_item.children():
+                itemslist.append(child)
+            # populate submenu
+            # no need to include stuff that's already in itemlist
+            while menuitem.level > 1:
+                # populate and sort the submenu children
+                if menuitem.level == 2:
+                    # activeitem is used to show active item in the sidebar
+                    activeitem = menuitem
+                    # get siblings of current menu item
+                    for sibling in menuitem.siblings():
+                       childlist.append(sibling)
+                    childlist.append(activeitem)
+                    # sort the list
+                    childlist.sort(key=lambda x: x.rank)
+                    # get the parent, though it isn't shown it is used for display purposes
+                    menuitem = menuitem.parent
+                    # don't change this, the parent has to be at the beginning of the list
+                    childlist.insert(0, menuitem)
+                # if we're deeper than level 2, go up but don't add the item to the list
+                elif menuitem.level > 2:
+                    menuitem = menuitem.parent
+            return render_to_string('treemenus/sidebar_fragment.html', {'itemslist': itemslist, 'childlist': childlist, 'activeitem': activeitem})
+        except:
+            pass
 
 register.filter('generate_sidebar', generate_sidebar)
 
