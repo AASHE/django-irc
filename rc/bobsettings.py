8d331818763e5186fc199851cd5b0372bcc84278
Robert Erb <rerb@users.noreply.github.com>
2012-05-14 11:45:05 -0400

    Changed default db to mysql.

--- rc/bobsettings.py
+++ rc/bobsettings.py
@@ -5,3 +5,14 @@ INSTALLED_APPS = INSTALLED_APPS + ('django_extensions',
 				   'template_repl')
 
 INTERNAL_IPS = ('127.0.0.1',)
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'django_irc_dev',
+        'USER': 'root',
+        'PASSWORD': '',
+        'HOST': 'localhost',
+        'PORT': '',
+        }
+    }
