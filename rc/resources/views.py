8648be07749d3cfe732e9f9dc0ed888a422256ee
Robert Erb <rerb@users.noreply.github.com>
2012-05-18 00:26:02 -0400

    merge changes

--- rc/resources/views.py
+++ rc/resources/views.py
@@ -1,3 +1,4 @@
+from django.core.urlresolvers import reverse
 from django.views.generic import ListView
 
 
@@ -6,9 +7,14 @@ class ResourceItemListView(ListView):
     def get_context_data(self, **kwargs):
         context = super(ResourceItemListView, self).get_context_data(
             **kwargs)
+        opts = self.model._meta
 
         if not 'title' in context.keys():
-            context['title'] = self.model._meta.verbose_name_plural.title()
+            context['title'] = opts.verbose_name_plural.title()
+
+        admin_change_url_name = 'admin:%s_%s_changelist' % (opts.app_label,
+                                                            opts.object_name.lower())
+        context['admin_changelist_url'] = reverse(admin_change_url_name)
 
         # So we can pass values from urls.py to templates:
         if self.kwargs:
