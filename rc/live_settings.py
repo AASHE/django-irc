deae434352d0bca14e493e244d8103402c9fa514
Jesse Legg <jesse.legg@aashe.org>
2012-05-29 10:20:23 -0400

    Updated production settings file to use proper STATIC_URL

--- rc/live_settings.py
+++ rc/live_settings.py
@@ -13,3 +13,12 @@ DATABASES = {
     }
 
 STATIC_ROOT = os.path.join(SITE_ROOT, '../static')
+
+# URL prefix for static files.
+# Example: "http://media.lawrence.com/static/"
+STATIC_URL = '/resources/static/'
+
+# URL prefix for admin static files -- CSS, JavaScript and images.
+# Make sure to use a trailing slash.
+# Examples: "http://foo.com/static/admin/", "/static/admin/".
+ADMIN_MEDIA_PREFIX = '/resources/static/admin/'
