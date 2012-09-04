0387cebc44c53f3631ff2681c6eb56d839bb0046
Matt Thomas <mthomas@aashe.org>
2012-09-04 13:12:51 -0400

    reworking members-only behaviors

--- rc/resources/views.py
+++ rc/resources/views.py
@@ -1,6 +1,10 @@
 from django.core.urlresolvers import reverse
 from django.views.generic import ListView
 from django.utils.decorators import classonlymethod
+from django.conf import settings
+from aashe.aasheauth.middleware import SESSION_USER_DICT_KEY
+
+MEMBER_ROLES = getattr(settings, 'AASHEAUTH_MEMBER_ROLES', ('Member', 'Guest', 'Staff'))
 
 class ResourceItemListView(ListView):
 
@@ -21,3 +25,22 @@ class ResourceItemListView(ListView):
             context.update(self.kwargs)
 
         return context
+    
+    def get_template_names(self):
+        member = False
+        
+        if self.request.session.has_key(SESSION_USER_DICT_KEY):
+            user_dict = request.session.get(SESSION_USER_DICT_KEY)
+            for role in MEMBER_ROLES:
+                if role in user_dict['roles'].values():
+                    member = True
+        
+        try: 
+          if not member and self.kwargs['member_only']:
+              base_template = 'aashe/base_membersonly.html'
+              model_template = "%s_membersonly.html" % self.context_object_name
+              return[base_template, model_template];
+          else:
+              return super(ResourceItemListView, self).get_template_names()
+        except:
+          return super(ResourceItemListView, self).get_template_names()
