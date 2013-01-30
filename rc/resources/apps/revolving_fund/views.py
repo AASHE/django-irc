a9e7c4257a2eaa9079757154d10a6667e134530e
Jesse Legg <jesse.legg@aashe.org>
2013-01-30 11:42:39 -0500

    Working pagination on search page

--- rc/resources/apps/revolving_fund/views.py
+++ rc/resources/apps/revolving_fund/views.py
@@ -3,6 +3,7 @@ from django.db.models import Sum
 from django.views.generic.list import ListView
 from django.views.generic.detail import DetailView
 from haystack.views import SearchView
+from aashe.utils.paginator import DiggPaginator as Paginator
 from models import RevolvingLoanFund
 
 
@@ -113,3 +114,31 @@ class FundSearchView(SearchView):
         extra['billion_amount'] = qs.filter(billion_dollar=True).aggregate(Sum('total_funds'))['total_funds__sum']
         extra['states'] = RevolvingLoanFund.objects.values_list('institution__state', flat=True).distinct().order_by('institution__state')        
         return extra
+
+    def build_page(self):
+        """
+        Paginates the results appropriately.
+
+        In case someone does not want to use Django's built-in pagination, it
+        should be a simple matter to override this method to do what they would
+        like.
+        """
+        try:
+            page_no = int(self.request.GET.get('page', 1))
+        except (TypeError, ValueError):
+            raise Http404("Not a valid number for page.")
+
+        if page_no < 1:
+            raise Http404("Pages should be 1 or greater.")
+
+        start_offset = (page_no - 1) * self.results_per_page
+        self.results[start_offset:start_offset + self.results_per_page]
+
+        paginator = Paginator(self.results, self.results_per_page)
+
+        try:
+            page = paginator.page(page_no)
+        except InvalidPage:
+            raise Http404("No such page!")
+
+        return (paginator, page)        
