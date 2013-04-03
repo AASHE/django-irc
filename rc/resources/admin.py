fefcee67e16fdf5cc290dc049036c5214b91d726
Matt Thomas <mthomas@aashe.org>
2013-04-03 09:20:15 -0400

    adding updated date to resource item admin

--- rc/resources/admin.py
+++ rc/resources/admin.py
@@ -3,7 +3,7 @@ from aashe.organization.models import Organization
 
 
 class ResourceItemAdmin(admin.ModelAdmin):
-    list_display = ('__unicode__', 'organization', 'notes', 'url', 'published')
+    list_display = ('__unicode__', 'organization', 'notes', 'url', 'updated_date','published')
     list_filter = ('published',)
     list_editable = ['published']
     search_fields = ['title', 'organization__name', 'description', 'notes']
