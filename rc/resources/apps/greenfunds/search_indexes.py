35d85e31d14345ee84bb99e8059a77f97504b6b6
Matt Thomas <mthomas@aashe.org>
2013-08-05 23:11:38 -0400

    adding back in indexes

--- rc/resources/apps/greenfunds/search_indexes.py
+++ rc/resources/apps/greenfunds/search_indexes.py
@@ -3,14 +3,14 @@ from haystack.indexes import *
 from haystack import site
 from models import GreenFund, StudentFeeFund, DonationFund, DepartmentFund
 
-# class StudentGreenFundIndex(SearchIndex):
-#     text = CharField(document=True, use_template=True)
-#     institution = CharField(model_attr='institution__name')
-#     fund_name = CharField(model_attr='fund_name')
-#     state = CharField(model_attr='institution__state')
-#     city = CharField(model_attr='institution__city')
+class StudentGreenFundIndex(SearchIndex):
+    text = CharField(document=True, use_template=True)
+    institution = CharField(model_attr='institution__name')
+    fund_name = CharField(model_attr='fund_name')
+    state = CharField(model_attr='institution__state')
+    city = CharField(model_attr='institution__city')
 
-#     def index_queryset(self):
-#         return StudentGreenFund.objects.filter(published=True)
+    def index_queryset(self):
+        return StudentGreenFund.objects.filter(published=True)
 
-# site.register(StudentGreenFund, StudentGreenFundIndex)
+site.register(StudentGreenFund, StudentGreenFundIndex)
