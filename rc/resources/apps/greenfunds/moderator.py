dc6ac0062f0f7c3d85d30f19873096b862c979dc
Matt Thomas <mthomas@aashe.org>
2013-08-13 14:23:26 -0400

    adding greenfunds moderators

--- rc/resources/apps/greenfunds/moderator.py
+++ rc/resources/apps/greenfunds/moderator.py
@@ -1,11 +1,24 @@
 from django.conf import settings
 from aashe.moderation import moderation
 from aashe.moderation.moderator import DefaultModerator
-# from forms import GreenFundCreateForm, GreenFundUpdateForm
+from forms import *
 
 
-# class GreenFundModerator(DefaultModerator):
-#     moderators = getattr(settings, 'MODERATORS', None)
-#     request_attr = 'request'
-# moderation.register([GreenFundCreateForm, GreenFundUpdateForm],
-#                     GreenFundModerator)
+class GreenFundModerator(DefaultModerator):
+    moderators = getattr(settings, 'MODERATORS', None)
+    request_attr = 'request'
+
+moderation.register(StudentFeeFundCreateForm,
+                    GreenFundModerator)
+
+moderation.register(DonationFundCreateForm,
+                    GreenFundModerator)
+
+moderation.register(DepartmentFundCreateForm,
+                    GreenFundModerator)
+
+moderation.register(HybridFundCreateForm,
+                    GreenFundModerator)
+
+moderation.register(GreenFundUpdateForm,
+                    GreenFundModerator)
